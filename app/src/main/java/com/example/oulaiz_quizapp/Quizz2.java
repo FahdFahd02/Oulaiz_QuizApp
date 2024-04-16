176fed97ba976ad92e0e8bc510fb9d97e31367b5
OulaizMohamedFahd <118827183+FahdFahd02@users.noreply.github.com>
2024-04-16 10:16:22 +0200

    Updated Quizz2

--- app/src/main/java/com/example/oulaiz_quizapp/Quizz2.java
+++ app/src/main/java/com/example/oulaiz_quizapp/Quizz2.java
@@ -15,7 +15,7 @@ public class Quizz2 extends AppCompatActivity {
     RadioButton rb;
     Button bNext;
     int score;
-    String RepCorrect="À droite";
+    String RepCorrect="A droite";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
